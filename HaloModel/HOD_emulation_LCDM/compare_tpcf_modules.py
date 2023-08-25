c359cdf16842b93c76ff21aed6b5505f720bab5a
Vikenes <vetle.vkns@gmail.com>
2023-08-25 11:58:04 +0200

    added comparison plot between halotools and pycorr tpcf

--- HaloModel/HOD_emulation_LCDM/compare_tpcf_modules.py
+++ HaloModel/HOD_emulation_LCDM/compare_tpcf_modules.py
@@ -3,8 +3,10 @@ import h5py
 import time 
 from pathlib import Path
 import pandas as pd
-import matplotlib.pyplot as plt
+
 import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
 matplotlib.rcParams['text.usetex'] = True
 matplotlib.rcParams.update({'font.size': 14})
 matplotlib.rcParams.update({'axes.labelsize': 20})
@@ -18,12 +20,10 @@ plt.rcParams.update(params)
 
 from compute_tpcf import compute_TPCF_fiducial_halocat_halotools, compute_TPCF_fiducial_halocat
 
-HOD_DATA_PATH = "/mn/stornext/d5/data/vetleav/HOD_AbacusData/c000_LCDM_simulation"
-HALO_ARRAYS_PATH = f"{HOD_DATA_PATH}/version0"
 
-INDATAPATH = f"{HOD_DATA_PATH}/HOD_data"
-OUTDATAPATH = f"{HOD_DATA_PATH}/data_measurements"
-dataset_names = ['train', 'test', 'val']
+FIGPATH = "plots"
+global SAVE
+SAVE = True 
 
 def compare_TPCF_times(iterations=10, threads=12, package='pycorr'):
     times = np.empty(iterations)
@@ -45,22 +45,74 @@ def compare_TPCF_times(iterations=10, threads=12, package='pycorr'):
 
 
 
-def compare_TPCF_fiducial_halocat(n_bins=128):
-    r_pycorr, xi_pycorr         = compute_TPCF_fiducial_halocat(n_bins=n_bins)
-    r_halotools, xi_halotools   = compute_TPCF_fiducial_halocat_halotools(n_bins=n_bins)
+def compare_TPCF_fiducial_plot(n_bins=128, threads=12):
+    r_halotools, xi_halotools, duration_halotools = compute_TPCF_fiducial_halocat_halotools(n_bins=n_bins, threads=threads)
+    r_pycorr, xi_pycorr, duration_pycorr          = compute_TPCF_fiducial_halocat(n_bins=n_bins, threads=threads)
+    # r_halotools, xi_halotools, duration_halotools = compute_TPCF_fiducial_halocat(n_bins=n_bins, threads=threads)
+    # r_halotools, xi_halotools = r_pycorr, xi_pycorr - 1e-4
 
-    plt.title(rf"Comparing TPCF packages, using {n_bins} values of $r\in[0.1,\,130]$")
-    plt.plot(r_pycorr, xi_pycorr, label='pycorr')
-    plt.plot(r_halotools, xi_halotools, '--', label='halotools')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.legend()
-    plt.show()
 
-# read_hdf5_files()
-# read_csv_original()
-# compute_TPCF_fiducial_halocat(n_bins=64)
-# compute_TPCF_train_halocats_halotools(n_bins=64)
-# compute_TPCF_fiducial_halocat_halotools(n_bins=64)
-# compare_TPCF_fiducial_halocat(n_bins=64)
+
+    fig = plt.figure(figsize=(8.0, 8.0))
+
+    gs = gridspec.GridSpec(
+        2, 1,
+        hspace=0,
+        height_ratios=[2, 1]
+    )
+
+    ax0 = plt.subplot(gs[0])
+    ax1 = plt.subplot(gs[1])
+
+    ax0.set_xscale('log')
+    ax0.xaxis.set_ticklabels([])
+    ax1.set_xscale('log')
+    ax0.set_yscale('log')
+    ax1.set_yscale('log')
+
+
+    ax0.plot(
+        r_pycorr, 
+        xi_pycorr, 
+        's-',
+        lw=2,
+        ms=4,
+        color='blue', 
+        label='pycorr'
+        )
+    ax0.plot(
+        r_halotools, 
+        xi_halotools, 
+        # 'x-',
+        ls='dashed', 
+        marker='|',
+        lw=1,
+        ms=16,
+        color='red', 
+        # alpha=0.7, 
+        label='halotools'
+        )
+
+   
+    ax1.plot(
+        r_pycorr, 
+        np.abs(xi_pycorr/xi_halotools - 1), 
+        color='k', 
+        )
+    
+    fig.suptitle(rf"$\mathrm{{Comparing\: TPCF\: packages, using\:}} {n_bins} \mathrm{{\:bins\: of\:}} r\in[0.1,\,130]$")
+
+
+    ax0.set_ylabel(r"$\xi_{gg}(r)$")
+    ax1.set_ylabel(r"$|\xi_{\rm pycorr}/\xi_{\rm halotools} - 1|$")
+    ax1.set_xlabel(r"$r\: [h^{-1} \mathrm{Mpc}]$")
+
+    ax0.legend()
+    fig.tight_layout()
+    if SAVE:
+        plt.savefig(f"{FIGPATH}/compare_TPCF_{n_bins}bins_halotools_pycorr.png", dpi=300)
+    else:
+        plt.show()
+
 # compare_TPCF_times(iterations=10, threads=128, package='pycorr')
+compare_TPCF_fiducial_plot(n_bins=64, threads=128)
