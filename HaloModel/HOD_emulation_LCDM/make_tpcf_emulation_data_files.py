09f8b35a22b3a744885c91488dd3d33a6461a420
Vikenes <vetle.vkns@gmail.com>
2023-09-07 12:16:15 +0200

    minor adjustments

--- HaloModel/HOD_emulation_LCDM/make_tpcf_emulation_data_files.py
+++ HaloModel/HOD_emulation_LCDM/make_tpcf_emulation_data_files.py
@@ -1,7 +1,7 @@
 import numpy as np 
 from pathlib import Path
 import pandas as pd
-import matplotlib.pyplot as plt
+import h5py
 
 """
 Make csv files with TPCF data for each node in each data set, with columns:
@@ -16,17 +16,18 @@ Make csv files with TPCF data for each node in each data set, with columns:
 """
 
 LCDM_PATH = Path("/mn/stornext/d5/data/vetleav/HOD_AbacusData/c000_LCDM_simulation")
-TPCF_INPATH = Path(LCDM_PATH / "data_measurements")
+TPCF_DATAPATH = Path(LCDM_PATH / "data_measurements")
 TPCF_OUTPATH = Path(LCDM_PATH / "emulation_data")
 PARAMS_INPATH = Path(LCDM_PATH / "HOD_data")
 
-data_set_names = ["train", "test", "val"]
+dataset_names = ["train", "test", "val"]
+
 
 def slice_TPCF_arrays_at_r_interval(r_low=0.6, r_high=60):
-    for flag in data_set_names:
-        NPY_FILES_TRAIN = TPCF_INPATH.glob(f"TPCF_{flag}_node*115bins*.npy")
+    for flag in dataset_names:
+        NPY_FILES_TRAIN = TPCF_DATAPATH.glob(f"TPCF_{flag}_node*115bins*.npy")
 
-        r_common = np.load(f"{TPCF_INPATH}/TPCF_{flag}_node0_115bins_ng_fixed.npy")[0]
+        r_common = np.load(f"{TPCF_DATAPATH}/TPCF_{flag}_node0_115bins_ng_fixed.npy")[0]
         r_mask_low_limit  = r_common > r_low
         r_mask_high_limit = r_common < r_high
         r_mask = r_mask_low_limit * r_mask_high_limit
@@ -49,6 +50,33 @@ def slice_TPCF_arrays_at_r_interval(r_low=0.6, r_high=60):
 
         out_df = pd.concat(_out_list)
         out_df.to_csv(TPCF_OUTPATH / f"TPCF_{flag}.csv", index=False)
+
+
+def hdf5_to_csv():
+    for flag in dataset_names:
+    ### Create csv file from hdf5 file 
+    
+        file_tpcf_h5py = h5py.File(f'{TPCF_OUTPATH}/TPCF_{flag}.hdf5', 'r')
+
+        _out_list = []
+        for key in file_tpcf_h5py.keys():
+            _out_list.append(pd.DataFrame({
+                'sigma_logM': file_tpcf_h5py[key].attrs['sigma_logM'],
+                'alpha'     : file_tpcf_h5py[key].attrs['alpha'],
+                'kappa'     : file_tpcf_h5py[key].attrs['kappa'],
+                'log10M1'   : file_tpcf_h5py[key].attrs['log10M1'],
+                'log10Mmin' : file_tpcf_h5py[key].attrs['log10Mmin'],
+                'r'         : file_tpcf_h5py[key]['r'][...],
+                'xi'        : file_tpcf_h5py[key]['xi'][...],
+            }))
+            
+        df_all = pd.concat(_out_list)
+        df_all.to_csv(
+            f'{TPCF_OUTPATH}/_TPCF_{flag}.csv',
+            index=False,
+        )
+        
+        file_tpcf_h5py.close()
 
 
 slice_TPCF_arrays_at_r_interval()
