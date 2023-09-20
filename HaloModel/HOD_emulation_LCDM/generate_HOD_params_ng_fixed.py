3467c3049354c16b51d3a2fa7d6ad231a7e6087d
Vikenes <vetle.vkns@gmail.com>
2023-09-20 11:16:38 +0200

    made gal.num.dens constraint more efficient. Clean up param-generating, move unfixed ng to this file as an option

--- HaloModel/HOD_emulation_LCDM/generate_HOD_params_ng_fixed.py
+++ HaloModel/HOD_emulation_LCDM/generate_HOD_params_ng_fixed.py
@@ -1,13 +1,10 @@
 import numpy as np
-import sys
 import pandas as pd
+import time 
 from smt.sampling_methods import LHS
 from pathlib import Path
 import hmd 
-from dq import Cosmology
-from hmd.catalogue import ParticleCatalogue, HaloCatalogue, GalaxyCatalogue
-from scipy import special
-from scipy.integrate import simpson
+# from typing import 
 
 from numdens_HOD import estimate_log10Mmin_from_gal_num_density
 
@@ -24,73 +21,87 @@ Ranges we consider for the five parameters h, omega_m, As, ns.
 """
 
 
-### FIDUCIAL VALUES. From https://arxiv.org/pdf/2208.05218.pdf, Table 3  ###
-# log10Mmin   = 13.62 # h^-1 Msun
-sigma_logM  = 0.6915 
-log10M1     = 14.42 # h^-1 Msun
-kappa       = 0.51 
-alpha       = 0.9168  
 
-ng_desired = 2.174e-4 # h^3 Mpc^-3
+def make_csv_files(
+        num_train:  int  = 50, 
+        num_test:   int  = 10, 
+        num_val:    int  = 10, 
+        fix_ng:     bool = True
+        ):
+    """
+    Create parameter datasets.
+    N=50 combinations for train,
+    N=10 combinations for test and validation
+    """
 
-# Vary parameters by 20% around fiducial values 
-param_limits = np.array([
-    [sigma_logM * 0.9,  sigma_logM  * 1.1],
-    [log10M1    * 0.9,  log10M1     * 1.1],
-    [kappa      * 0.9,  kappa       * 1.1],
-    [alpha      * 0.9,  alpha       * 1.1],
-])
+    ### FIDUCIAL VALUES. From https://arxiv.org/pdf/2208.05218.pdf, Table 3  ###
+    # log10Mmin   = 13.62 # h^-1 Msun
+    sigma_logM  = 0.6915 
+    log10M1     = 14.42 # h^-1 Msun
+    kappa       = 0.51 
+    alpha       = 0.9168  
 
-"""
-Create parameter datasets.
-N=50 combinations for train,
-N=10 combinations for test and validation
-"""
-num_train   = int(50)
-num_test    = int(10)
-num_val     = int(10)
+    ng_desired = 2.174e-4 # h^3 Mpc^-3
 
-dataset_config_size = {
-    'train': num_train,
-    'test':  num_test,
-    'val':   num_val
-}
-dataset_names = ['train', 'test', 'val']
+    # Vary parameters by 20% around fiducial values 
+    param_limits = np.array([
+        [sigma_logM * 0.9,  sigma_logM  * 1.1],
+        [log10M1    * 0.9,  log10M1     * 1.1],
+        [kappa      * 0.9,  kappa       * 1.1],
+        [alpha      * 0.9,  alpha       * 1.1],
+    ])
 
+    if not fix_ng:
+        log10Mmin           = 13.62 # h^-1 Msun
+        log10Mmin_limits    = np.array([log10Mmin * 0.9, log10Mmin * 1.1])
+        param_limits        = np.vstack((log10Mmin_limits, param_limits))
 
-# Create parameter files. 
-for dataset in dataset_names:
-    # Create LHS sampler
-    LHC_sampler = LHS(
-        xlimits=param_limits, 
-        criterion="corr",
-        random_state=RANDOMSTATE,
-    ) 
-    
-    # Sample parameters
-    samples = dataset_config_size[dataset]
-    node_params = LHC_sampler(samples)
+    dataset_config_size = {
+        'train': num_train,
+        'test':  num_test,
+        'val':   num_val
+        }
+    dataset_names = ['train', 'test', 'val']
 
-    log10Mmin_best_fit = estimate_log10Mmin_from_gal_num_density(
-        sigma_logM_array=node_params[:, 0],
-        log10M1_array=node_params[:, 1],
-        kappa_array=node_params[:, 2],
-        alpha_array=node_params[:, 3],
-        ng_desired=ng_desired,
-    )
+    # Create parameter files. 
+    for dataset in dataset_names:
 
+        # Create LHS sampler
+        LHC_sampler = LHS(
+            xlimits      = param_limits, 
+            criterion    = "corr",
+            random_state = RANDOMSTATE,
+        ) 
+        
+        # Sample parameters
+        samples     = dataset_config_size[dataset]
+        node_params = LHC_sampler(samples)
+        if fix_ng:
+            start       = time.time() 
+            log10Mmin   = estimate_log10Mmin_from_gal_num_density(
+                sigma_logM_array    = node_params[:, 0],
+                log10M1_array       = node_params[:, 1],
+                kappa_array         = node_params[:, 2],
+                alpha_array         = node_params[:, 3],
+                ng_desired          = ng_desired,
+                )
 
-    # Save parameters to csv file
-    df = pd.DataFrame({
-        'log10Mmin'     : log10Mmin_best_fit,
-        'sigma_logM'    : node_params[:, 0],
-        'log10M1'       : node_params[:, 1],
-        'kappa'         : node_params[:, 2],
-        'alpha'         : node_params[:, 3],
-    })
-    fname = Path("HOD_parameters_ng_fixed_" + str(dataset) + ".csv")
-    outfile = Path(OUTFILEPATH / fname)
-    df.to_csv(
-        outfile,
-        index=False
-    )
+            node_params = np.hstack((log10Mmin[:, np.newaxis], node_params))
+            print(f"Estimating log10Mmin for {dataset} dataset took {time.time() - start:.2f} seconds.")
+        
+        # Save parameters to csv file
+        df = pd.DataFrame({
+            'log10Mmin'     : node_params[:, 0],
+            'sigma_logM'    : node_params[:, 1],
+            'log10M1'       : node_params[:, 2],
+            'kappa'         : node_params[:, 3],
+            'alpha'         : node_params[:, 4],
+        })
+        fname   = Path("HOD_parameters_ng_fixed_" + str(dataset) + ".csv")
+        outfile = Path(OUTFILEPATH / fname)
+        df.to_csv(
+            outfile,
+            index=False
+        )
+
+make_csv_files(fix_ng=False)
--- HaloModel/HOD_emulation_LCDM/numdens_HOD.py
+++ HaloModel/HOD_emulation_LCDM/numdens_HOD.py
@@ -2,22 +2,26 @@ import numpy as np
 import h5py 
 import time 
 from dq import Cosmology
-import hmd
-from hmd.catalogue import ParticleCatalogue, HaloCatalogue, GalaxyCatalogue
-from hmd.occupation import Zheng07Centrals, Zheng07Sats
-from hmd.galaxy import Galaxy
-from hmd.profiles import FixedCosmologyNFW
-from hmd.populate import HODMaker
+# import hmd
+from hmd.concentration import diemer15
+from hmd.catalogue import HaloCatalogue
+
 from pathlib import Path
 from scipy import special
 from scipy.integrate import simpson
 import pandas as pd
 from scipy.interpolate import UnivariateSpline
 
-import matplotlib.pyplot as plt
-
 import warnings
 warnings.filterwarnings("ignore", message='Astropy cosmology class contains massive neutrinos, which are not taken into account in Colossus.')
+
+"""
+Constrain log10Mmin from ng
+Used when generating HOD parameter sets. 
+For each parameter set, we estimate log10Mmin that yields ng = ng_desired.
+"""
+
+
 
 HOD_DATA_PATH = "/mn/stornext/d5/data/vetleav/HOD_AbacusData/c000_LCDM_simulation"
 HALO_ARRAYS_PATH = f"{HOD_DATA_PATH}/version0"
@@ -26,6 +30,10 @@ OUTFILEPATH = f"{HOD_DATA_PATH}/HOD_data"
 dataset_names = ['train', 'test', 'val']
 
 def compute_gal_num_density():
+    """
+    Used for testing the ng computation only.
+    Compares the analytical ng with the ng computed from the halo catalogue.
+    """
 
     log10Mmin   = 13.62 # h^-1 Msun
     sigma_logM  = 0.6915
@@ -48,10 +56,11 @@ def compute_gal_num_density():
         vel,
         mass,
         boxsize,
-        conc_mass_model=hmd.concentration.diemer15,
+        conc_mass_model=diemer15,
         cosmology=cosmology,
         redshift=redshift,
         )
+    
     N = 250
     mmin = np.log10(np.min(mass))
     mmax = np.log10(np.max(mass))
@@ -89,20 +98,24 @@ def compute_gal_num_density():
     print(f"nc_bar: {nc_estimated:.4e} | {nc_analytical:.4e} | {np.abs(nc_estimated - nc_analytical):.4e} ")
     print(f"ns_bar: {ns_estimated:.4e} | {ns_analytical:.4e} | {np.abs(ns_estimated - ns_analytical):.4e} ")
     
+
+
 def compute_ng_analytical(log10Mmin, sigma_logM, log10M1, kappa, alpha, mass_center, dn_dM):
+    """
+    Computes the analytical ng from HOD parameters and HMF.
+    log10Mmin can either be scalar or a 1D array. 
+    """
     ### Compute ng analytically from HOD params and HMF
+    mass_center         = mass_center[:,np.newaxis]
+    Nc                  = 0.5 * special.erfc(np.log10(10**log10Mmin / mass_center) / sigma_logM)
+    mask                = mass_center > kappa * 10**log10Mmin # No satellite galaxies below Mmin
 
-    Nc = 0.5 * special.erfc(np.log10(10**log10Mmin / mass_center) / sigma_logM)
-    mask = mass_center > kappa * 10**log10Mmin
-    lambda_values = np.zeros_like(mass_center)
-    lambda_values[mask] = ((mass_center[mask] - kappa * 10**log10Mmin) / 10**log10M1)**alpha
-
+    lambda_values = ((mass_center - kappa * 10**log10Mmin) / 10**log10M1)**alpha 
+    lambda_values[mask==False] = 0.0
     Ns = Nc * lambda_values
-    Ng = Nc + Ns
 
-    integrand = dn_dM * Ng
+    return simpson(dn_dM[:,np.newaxis] * (Nc + Ns), mass_center, axis=0)
 
-    return simpson(integrand, mass_center)
 
 
 def estimate_log10Mmin_from_gal_num_density(
@@ -117,75 +130,69 @@ def estimate_log10Mmin_from_gal_num_density(
 
     ### Make halo catalogue 
     # Define cosmology and simparams 
-    cosmology = Cosmology.from_custom(run=0, emulator_data_path=HOD_DATA_PATH)
-    redshift = 0.25 
-    boxsize  = 2000.0
+    cosmology   = Cosmology.from_custom(run=0, emulator_data_path=HOD_DATA_PATH)
+    redshift    = 0.25 
+    boxsize     = 2000.0
     # Halo data 
     pos  = np.load(f"{HALO_ARRAYS_PATH}/L1_pos.npy") 
     vel  = np.load(f"{HALO_ARRAYS_PATH}/L1_vel.npy")
     mass = np.load(f"{HALO_ARRAYS_PATH}/L1_mass.npy")
     # Make halo catalogue
+    # Used to compute the HMF which is needed to compute ng
     halocat = HaloCatalogue(
         pos,
         vel,
         mass,
         boxsize,
-        conc_mass_model=hmd.concentration.diemer15,
+        conc_mass_model=diemer15,
         cosmology=cosmology,
         redshift=redshift,
         )
     
     # Compute HMF from halo catalogue 
-    N = 200
-    mmin = np.log10(np.min(mass))
-    mmax = np.log10(np.max(mass))
-    mass_edges = np.logspace(mmin, mmax, N)
-    dn_dM = halocat.compute_hmf(mass_edges)
+    N           = 201
+    mass_edges  = np.logspace(np.log10(np.min(mass)), np.log10(np.max(mass)), N)
+    dn_dM       = halocat.compute_hmf(mass_edges)
     mass_center = 0.5 * (mass_edges[1:] + mass_edges[:-1])
 
     # Estimate ng from halo catalogue
-    N_Mmin = 200
-    log10Mmin_arr = np.linspace(13.4, 13.9, N_Mmin) # Range yields ng values around 2.174e-4 for all parameter sets
-    
-    if test:
-        ### FIDUCIAL VALUES. From https://arxiv.org/pdf/2208.05218.pdf, Table 3  ###
-        # For testing purposes mainly. 
-        sigma_logM_array = np.array([0.6915])
-        log10M1_array    = np.array([14.42])
-        kappa_array      = np.array([0.51])
-        alpha_array      = np.array([0.9168])
+    # The log10Mmin range chosen has been tested. 
+    # it yields ng values around 2.174e-4 for all parameter sets.
+    N_Mmin        = 100
+    log10Mmin_arr = np.linspace(13.4, 13.9, N_Mmin) 
 
-
-    N_params = len(sigma_logM_array)
-    log10Mmin_best_fit = np.zeros(N_params)
+    # Number of parameter sets
+    N_params            = len(sigma_logM_array)
+    log10Mmin_best_fit  = np.zeros(N_params)
 
     # Loop through parameter sets and estimate log10Mmin that yields ng = ng_desired
     for i in range(N_params):
-        ng_arr = np.zeros_like(log10Mmin_arr)
-        sigma_logM  = sigma_logM_array[i]
-        log10M1     = log10M1_array[i]
-        kappa       = kappa_array[i]
-        alpha       = alpha_array[i]
+        # Compute ng for all values of log10Mmin
+        ng_arr          = compute_ng_analytical(log10Mmin_arr, 
+                                                sigma_logM_array[i], 
+                                                log10M1_array[i], 
+                                                kappa_array[i], 
+                                                alpha_array[i], 
+                                                mass_center, 
+                                                dn_dM)
+        
+        # Use spline interpolation to find log10Mmin for ng = ng_desired
+        # log10Mmin is a very well-behaved function 
+        ng_sorted_idx    = np.argsort(ng_arr)
+        log10Mmin_spline = UnivariateSpline(
+            ng_arr[ng_sorted_idx], 
+            log10Mmin_arr[ng_sorted_idx], 
+            k=3, 
+            s=0
+            )
+        log10Mmin_best_fit[i] = log10Mmin_spline(ng_desired)
 
-        # Compute ng for for all log10Mmin
-        for j in range(N_Mmin):
-            log10Mmin   = log10Mmin_arr[j]
-
-            ng_analytical = compute_ng_analytical(log10Mmin, sigma_logM, log10M1, kappa, alpha, mass_center, dn_dM)
-            ng_arr[j] = ng_analytical
-
-        # Interpolate to find log10Mmin that yields ng = ng_desired
-        # log10Mmin(n_g) is a very well-behaved function. 
-        ng_sorted_idx = np.argsort(ng_arr)
-
-        log10Mmin_spline = UnivariateSpline(ng_arr[ng_sorted_idx], log10Mmin_arr[ng_sorted_idx], k=3, s=0)
-        log10Mmin_best_fit_spline = log10Mmin_spline(ng_desired) # Final estiamte 
-        log10Mmin_best_fit[i] = log10Mmin_best_fit_spline
-
+        # Testing implementation accuracy 
+        # Commented out to save time
         # Compute ng for best fit log10Mmin, to check if it is close to ng_desired
-        ng_best_fit_spline = compute_ng_analytical(log10Mmin_best_fit_spline, sigma_logM, log10M1, kappa, alpha, mass_center, dn_dM)
-        if test:
-            print(f"Best fit [{i:2.0f}]: ng: {ng_best_fit_spline:.4e} | log10Mmin: {log10Mmin_best_fit_spline:.4f} | rel.diff. ng: {np.abs(1.0 - ng_best_fit_spline / ng_desired):.6e}")
+        # if test:
+        #     ng_best_fit_spline = compute_ng_analytical(log10Mmin_best_fit[i], sigma_logM, log10M1, kappa, alpha, mass_center, dn_dM)[0]
+        #     print(f"Best fit [{i:2.0f}]: ng: {ng_best_fit_spline:.4e} | log10Mmin: {log10Mmin_best_fit[i]:.4f} | rel.diff. ng: {np.abs(1.0 - ng_best_fit_spline / ng_desired):.6e}")
 
     return log10Mmin_best_fit
 
