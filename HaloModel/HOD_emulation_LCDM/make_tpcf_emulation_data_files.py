422b43913c06fccbaca5daded487f08cd30bccf7
Vikenes <vetle.vkns@gmail.com>
2023-09-25 14:07:32 +0200

    add option for storing log10 of data

--- HaloModel/HOD_emulation_LCDM/make_tpcf_emulation_data_files.py
+++ HaloModel/HOD_emulation_LCDM/make_tpcf_emulation_data_files.py
@@ -32,7 +32,8 @@ dataset_names = ["train", "test", "val"]
 
 
 
-def make_TPCF_HDF_files_arrays_at_sliced_r(r_low=0.6, r_high=60):
+def make_TPCF_HDF_files_arrays_at_sliced_r(r_low=0.6, r_high=60, 
+                                           log10=True, overwrite=False):
     """
     Create hdf5 files with TPCF data for each node in each data set.
     containing the HOD parameters for each node.
@@ -46,7 +47,7 @@ def make_TPCF_HDF_files_arrays_at_sliced_r(r_low=0.6, r_high=60):
     for flag in dataset_names:
         # Create hdf5 file
         outfile = f"{EMULATION_DATA_PATH}/TPCF_{flag}.hdf5"
-        if Path(outfile).exists():
+        if Path(outfile).exists() and not overwrite:
             print(f"File {outfile} already exists. Skipping.")
             # continue
         file_tpcf_h5py = h5py.File(outfile, 'w')
@@ -89,13 +90,24 @@ def make_TPCF_HDF_files_arrays_at_sliced_r(r_low=0.6, r_high=60):
             node_group.attrs['log10Mmin']   = parameters_df['log10Mmin'].iloc[node_idx]            
 
             # Store TPCF data in hdf5 file
-            node_group.create_dataset('r', data=r)
-            node_group.create_dataset('xi', data=xi)
+            if log10:
+                r_data_name     = 'log10r'
+                xi_data_name    = 'log10xi'
+                r_data          = np.log10(r)
+                xi_data         = np.log10(xi)
+            else:
+                r_data_name     = 'r'
+                xi_data_name    = 'xi'
+                r_data          = r
+                xi_data         = xi
+
+            node_group.create_dataset(r_data_name, data=r_data)
+            node_group.create_dataset(xi_data_name, data=xi_data)
 
         file_tpcf_h5py.close()
 
 
-def hdf5_to_csv():
+def hdf5_to_csv(log10=True):
     """
     Make csv files from hdf5 files.
     This is the data actually used for emulation. 
@@ -107,15 +119,24 @@ def hdf5_to_csv():
 
         _out_list = []
         for key in file_tpcf_h5py.keys():
-
+            if log10:
+                r_data_name     = 'log10r'
+                xi_data_name    = 'log10xi'
+                r_data          = np.log10(file_tpcf_h5py[key]['r'][...])
+                xi_data         = np.log10(file_tpcf_h5py[key]['xi'][...])
+            else:
+                r_data_name     = 'r'
+                xi_data_name    = 'xi'
+                r_data          = file_tpcf_h5py[key]['r'][...]
+                xi_data         = file_tpcf_h5py[key]['xi'][...]
             df = pd.DataFrame({
-                'sigma_logM': file_tpcf_h5py[key].attrs['sigma_logM'],
-                'alpha'     : file_tpcf_h5py[key].attrs['alpha'],
-                'kappa'     : file_tpcf_h5py[key].attrs['kappa'],
-                'log10M1'   : file_tpcf_h5py[key].attrs['log10M1'],
-                'log10Mmin' : file_tpcf_h5py[key].attrs['log10Mmin'],
-                'r'         : file_tpcf_h5py[key]['r'][...],
-                'xi'        : file_tpcf_h5py[key]['xi'][...],
+                'sigma_logM' : file_tpcf_h5py[key].attrs['sigma_logM'],
+                'alpha'      : file_tpcf_h5py[key].attrs['alpha'],
+                'kappa'      : file_tpcf_h5py[key].attrs['kappa'],
+                'log10M1'    : file_tpcf_h5py[key].attrs['log10M1'],
+                'log10Mmin'  : file_tpcf_h5py[key].attrs['log10Mmin'],
+                r_data_name  : r_data,
+                xi_data_name : xi_data,
             })
             _out_list.append(df)
         
@@ -128,5 +149,5 @@ def hdf5_to_csv():
         file_tpcf_h5py.close()
 
 # slice_TPCF_arrays_at_r_interval()
-make_TPCF_HDF_files_arrays_at_sliced_r()
-hdf5_to_csv()
+# make_TPCF_HDF_files_arrays_at_sliced_r()
+hdf5_to_csv(log10=True)
