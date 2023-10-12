1dfb76d58878d86066be37e5a6ba75fc59d6a798
Vikenes <vetle.vkns@gmail.com>
2023-10-12 17:05:21 +0200

    halo catalogues for all versions have been made

--- HaloModel/HOD_and_cosmo_emulation/make_HOD.py
+++ HaloModel/HOD_and_cosmo_emulation/make_HOD.py
@@ -12,47 +12,63 @@ from pathlib import Path
 import pandas as pd
 import sys 
 
-# import warnings
-# warnings.filterwarnings("ignore")
+import concurrent.futures
 
-DATA_PATH           = "/mn/stornext/d5/data/vetleav/HOD_AbacusData/c000_LCDM_simulation"
-HALO_ARRAYS_PATH    = f"{DATA_PATH}/version0"
-HOD_DATA_PATH       = f"{DATA_PATH}/TPCF_emulation"
-HOD_PARAMETERS_PATH = f"{HOD_DATA_PATH}/HOD_parameters"
-OUTFILEPATH         = f"{HOD_DATA_PATH}/HOD_catalogues"
+import warnings
+warnings.filterwarnings("ignore", message='Astropy cosmology class contains massive neutrinos, which are not taken into account in Colossus.')
+
+notes = []
+
+D13_BASE_PATH           = "/mn/stornext/d13/euclid_nobackup/halo/AbacusSummit"
 
 dataset_names = ['train', 'test', 'val']
 
-def make_hdf5_files(ng_fixed=True):
+def make_hdf5_files_single_version(
+        version:    int  = 0,
+        phase:      int  = 0,
+        ng_fixed:   bool = True
+        ):
+    print_every_n = 25 
 
-    print("Making hdf5 files...")
+    simname             = f"AbacusSummit_base_c{str(version).zfill(3)}_ph{str(phase).zfill(3)}"
+    HOD_DATA_PATH       = f"{D13_BASE_PATH}/emulation_files/{simname}"
+    OUTFILEPATH         = f"{HOD_DATA_PATH}/HOD_catalogues"
+    HOD_PARAMETERS_PATH = f"{HOD_DATA_PATH}/HOD_parameters"
+    HALO_ARRAYS_PATH    = f"{HOD_DATA_PATH}/pos_vel_mass_arrays" # directory with pos, vel, mass arrays
 
+    if not Path(HOD_DATA_PATH).exists():
+        print(f"Error: Simulation path {HOD_DATA_PATH} does not exist, aborting...")
+        raise FileNotFoundError
+
+    Path(OUTFILEPATH).mkdir(parents=True, exist_ok=True)
+
+    print(f"Making hdf5 files for {simname}...")
+    t0_total = time.time()
     for flag in dataset_names:
-        
-        print(f"Computing for flag={flag}")
+        # print(f"Computing for flag={flag}")
         filename_suffix = f"{flag}"
         if ng_fixed:
             filename_suffix += "_ng_fixed"
         
-        outfname = f"halocat_{filename_suffix}"
-
-        OUTFILE = Path((f"{OUTFILEPATH}/{outfname}.hdf5"))
+        outfname = f"halocat_{filename_suffix}.hdf5"
+        OUTFILE = Path((f"{OUTFILEPATH}/{outfname}"))
         if OUTFILE.exists():
+            # Prevent overwriting to save time 
             print(f"File {OUTFILE} already exists, skipping...")
             continue
+
         
         # Make hdf5 file
-        fff = h5py.File(f"{OUTFILEPATH}/{outfname}.hdf5", "w")
+        fff = h5py.File(f"{OUTFILEPATH}/{outfname}", "w")
 
         # Set up cosmology and simulation info 
-        cosmology = Cosmology.from_custom(run=0, emulator_data_path=DATA_PATH)
+        # Uses the cosmology.dat file found in the HOD_DATA_PATH
+        cosmology = Cosmology.from_custom(run=0, emulator_data_path=HOD_DATA_PATH)
         redshift  = 0.25 
         boxsize   = 2000.0
 
         # Store cosmology and simulation info in hdf5 file
         fff.attrs["boxsize"]            = boxsize
-        fff.attrs["central_galaxy"]     = 0
-        fff.attrs["satellite_galaxy"]   = 1
         fff.attrs["H0"]                 = float(cosmology.H0.value)
         fff.attrs["Om0"]                = cosmology.Om0
         fff.attrs["Ode0"]               = cosmology.Ode0
@@ -86,18 +102,25 @@ def make_hdf5_files(ng_fixed=True):
 
         # Loop over HOD parameters 
         # Populate halos with galaxies for each HOD parameter set 
+        notes.append("Test parallelization here vs over the versions")
+
+        t0_flag = time.time()
+        t0_flag_total = time.time()
         for node_idx in range(len(node_params_df)):
 
-            print(f"Running node{node_idx}...", end=" ")
-            t0 = time.time()
+            if node_idx % print_every_n == 0 and node_idx != 0 or node_idx == len(node_params_df) - 1:
+                print(f"{simname}-{flag}-nodes {node_idx-print_every_n}-{node_idx} complete.", end=" ")
+                print(f"Duration: {time.time() - t0_flag:.2f} sec.")
+                t0_flag = time.time()
+            
 
             # Store HOD parameters in hdf5 file
-            HOD_group = fff.create_group(f"node{node_idx}") 
-            HOD_group.attrs['log10Mmin']     = node_params_df['log10Mmin'].iloc[node_idx]
-            HOD_group.attrs['sigma_logM']    = node_params_df['sigma_logM'].iloc[node_idx]
-            HOD_group.attrs['log10M1']       = node_params_df['log10M1'].iloc[node_idx]
-            HOD_group.attrs['kappa']         = node_params_df['kappa'].iloc[node_idx]
-            HOD_group.attrs['alpha']         = node_params_df['alpha'].iloc[node_idx]
+            HOD_group                     = fff.create_group(f"node{node_idx}") 
+            HOD_group.attrs['log10Mmin']  = node_params_df['log10Mmin'].iloc[node_idx]
+            HOD_group.attrs['sigma_logM'] = node_params_df['sigma_logM'].iloc[node_idx]
+            HOD_group.attrs['log10M1']    = node_params_df['log10M1'].iloc[node_idx]
+            HOD_group.attrs['kappa']      = node_params_df['kappa'].iloc[node_idx]
+            HOD_group.attrs['alpha']      = node_params_df['alpha'].iloc[node_idx]
 
             # Populate halos with galaxies
             maker = HODMaker(
@@ -125,15 +148,15 @@ def make_hdf5_files(ng_fixed=True):
             galaxy_df           = maker.galaxy_df
 
             # Get number of central and satellite galaxies
-            # Compute number density of galaxies and store it in hdf5 file
             galaxy_df_central   = galaxy_df[galaxy_df['galaxy_type'] == 'central']
             galaxy_df_satellite = galaxy_df[galaxy_df['galaxy_type'] == 'satellite']
-            Ng = len(galaxy_df)
-            Nc = len(galaxy_df_central)
-            Ns = len(galaxy_df_satellite)
-            ng = Ng / (boxsize**3)
-            nc = Nc / (boxsize**3)
-            ns = Ns / (boxsize**3)
+
+            # Compute number density of galaxies and store it in hdf5 file
+            box_volume = boxsize**3
+            ng = len(galaxy_df) / box_volume
+            nc = len(galaxy_df_central) / box_volume
+            ns = len(galaxy_df_satellite) / box_volume
+
             HOD_group.attrs['ng'] = ng
             HOD_group.attrs['nc'] = nc
             HOD_group.attrs['ns'] = ns
@@ -159,10 +182,54 @@ def make_hdf5_files(ng_fixed=True):
                     data = galaxy_df[prop].values,
                     dtype= galaxy_df[prop].dtypes
                     )
-                
-            print(f"Finished, took {time.time() - t0:.2f} seconds.")
+            # if node_idx % print_every_n == 0 or node_idx == len(node_params_df) - 1:
+                # print(f"complete, duration: {time.time() - t0:.2f} sec.")
 
         fff.close()
+        print(f"{simname}-{flag} complete. Duration: {time.time() - t0_flag_total:.2f} sec.")
+
+    print(f"{simname} complete. Duration: {time.time() - t0_total:.2f} sec.")
+
+def make_hdf5_files_c000_all_phases(
+        parallel=True
+        ):
+    phases = np.arange(0,25)
+    t000 = time.time()
+    if parallel:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(make_hdf5_files_single_version, [0 for i in range(len(phases))], phases)
+        
+    else:
+        for ph in phases:
+            make_hdf5_files_single_version(
+                version=0,
+                phase=ph,
+                ng_fixed=True
+                )
+            
+    tot_dur = time.time() - t000
+    print(f"Total duration: {tot_dur:.2f} sec")
 
 
-make_hdf5_files(ng_fixed=True, pos_only=True)
+def make_hdf5_files_all_emulator_versions(parallel=True):
+    versions = np.arange(130, 182)
+    t000 = time.time()
+    if parallel:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(make_hdf5_files_single_version, [v for v in versions], [0 for v in versions])
+    print(f"Total duration: {time.time() - t000:.2f} sec")
+
+def make_hdf5_files_non_emulator_versions(parallel=True):
+    versions = np.arange(1,5)
+    versions = np.concatenate((versions, np.arange(100, 127)))
+    t000 = time.time()
+    if parallel:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(make_hdf5_files_single_version, [v for v in versions], [0 for v in versions])
+    print(f"Total duration: {time.time() - t000:.2f} sec")
+
+
+
+# make_hdf5_files_c000_all_phases(parallel=True)
+# make_hdf5_files_all_emulator_versions(parallel=True)
+# make_hdf5_files_non_emulator_versions(parallel=True)
