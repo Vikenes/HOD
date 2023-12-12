ae56b19135503aa26c8266e9243f7e0dc3e6be9b
Vikenes <vetle.vkns@gmail.com>
2023-12-12 11:57:14 +0100

    new plots

--- HaloModel/individual_HOD_parameter_variation/plot_occupation_numbers_varying_single_params.py
+++ HaloModel/individual_HOD_parameter_variation/plot_occupation_numbers_varying_single_params.py
@@ -14,6 +14,11 @@ params = {'xtick.top': True,
           }
 plt.rcParams.update(params)
 
+
+SAVE = False 
+
+
+
 def N_c(logM, alpha, kappa, sigma_logM, logMmin, logM1):
     erf_term = erf((logM - logMmin)/sigma_logM)
     return 0.5*(1.0 + erf_term)
@@ -52,54 +57,93 @@ def plot_varying_sigma_logM():
     fig, ax = plt.subplots(figsize=(10, 8))
     sigma_logM_array = np.linspace(0.1, 1, 10)
     for i, sigma_logM in enumerate(sigma_logM_array):
+        if i == 0 or i == len(sigma_logM_array)-1:
+            ls = "solid"
+            alpha_ = 1.0 
+        else:
+            ls = "dashed"
+            alpha_ = 0.7 
+        if i == len(sigma_logM_array)-1:
+            color_ = "blue"
+        else:
+            color_ = colors[i]
         nc = N_c(logM, alpha, kappa, sigma_logM, log10_Mmin, log10M1)
-        ax.plot(logM, nc,       color=colors[i], label=rf"$\sigma_{{\log M}}={sigma_logM:.1f}$")
+        ax.plot(logM, nc, ls=ls, alpha=alpha_, color=color_, label=rf"$\sigma_{{\log M}}={sigma_logM:.1f}$")
     fig.suptitle(r"Varying $\sigma_{\log M}$")
-    # ax.set_xlabel(r"$\log M$")
     ax.set_xlabel(XLABEL)
-
     ax.set_ylabel(r"$\langle N_c \rangle$")
     ax.legend(loc="best")
     return fig 
 
 
-def plot_varying_log10_Mmin():
-    fig, axes = plt.subplots(1,2, figsize=(17, 8))
-    ax0 = axes[0]
-    ax1 = axes[1]
+def plot_varying_log10_Mmin_Nc():
+    fig, ax = plt.subplots(figsize=(10, 8))
     log10_Mmin_array = np.linspace(12, 14, 11)
-    # ax0.plot([],[], label=r"$N_c$", color='k')
-    # ax1.plot([],[], '--', label=r"$N_s$", color='k')
     for i, log10_Mmin in enumerate(log10_Mmin_array):
+        if i == 0 or i == len(log10_Mmin_array)-1:
+            ls = "solid"
+            alpha_ = 1.0 
+        else:
+            ls = "dashed"
+            alpha_ = 0.7 
+        if i == len(log10_Mmin_array)-1:
+            color_ = "blue"
+        else:
+            color_ = colors[i]
         nc = N_c(logM, alpha, kappa, sigma_logM, log10_Mmin, log10M1)
+        ax.plot(logM, nc, ls=ls, alpha=alpha_, color=color_, label=rf"$\log_{{10}} M_\mathrm{{min}}={log10_Mmin:.1f}$")
+
+    ax.set_xlabel(XLABEL)
+    ax.set_ylabel(r"$\langle N_c \rangle$")
+    ax.set_title(r"$\langle N_c \rangle$")
+    ax.legend(loc='lower right', fontsize=12)
+    return fig 
+
+def plot_varying_log10_Mmin_Ns():
+    fig, ax = plt.subplots(figsize=(10, 8))
+    log10_Mmin_array = np.linspace(12., 14, 11)
+
+    for i, log10_Mmin in enumerate(log10_Mmin_array):
+        if i == 0 or i == len(log10_Mmin_array)-1:
+            ls = "solid"
+            alpha_ = 1.0 
+        else:
+            ls = "dashed"
+            alpha_ = 0.7 
+        if i == len(log10_Mmin_array)-1:
+            color_ = "blue"
+        else:
+            color_ = colors[i]
+
         ns = N_s(logM, alpha, kappa, sigma_logM, log10_Mmin, log10M1)
-        ax0.plot(logM, nc, color=colors[i], label=rf"$\log_{{10}} M_\mathrm{{min}}={log10_Mmin:.1f}$")
-        ax1.plot(logM, ns, color=colors[i], label=rf"$\log_{{10}} M_\mathrm{{min}}={log10_Mmin:.1f}$")
-        # ls = lambda_sat(logM, alpha, kappa, sigma_logM, log10_Mmin, log10M1)
-        # ax1.plot(logM, ls, '--', color=colors[i], label=rf"$\log_{{10}} M_\mathrm{{min}}={log10_Mmin:.1f}$")
+        ax.plot(logM, ns, ls=ls, alpha=alpha_, color=color_, label=rf"$\log_{{10}} M_\mathrm{{min}}={log10_Mmin:.1f}$")
 
 
-    fig.suptitle(r"Varying $\log_{10} M_\mathrm{min}$")
-    ax0.set_xlabel(XLABEL)
-    ax1.set_xlabel(XLABEL)
-    ax1.set_yscale('log')
-    ax0.set_ylabel(r"$\langle N_c \rangle$")
-    ax1.set_ylabel(r"$\langle N_s \rangle$")
-    ax1.set_ylim(1e-4, 7)
-    ax0.set_title(r"$\langle N_c \rangle$")
-    ax1.set_title(r"$\langle N_s \rangle$")
-    ax10 = ax0.get_xlim()[0]
-    ax0.set_xlim(ax10, 15.5)
-    ax0.legend(loc='lower right', fontsize=12)
-    ax1.legend(loc='upper left')
+    ax.set_xlabel(XLABEL)
+    ax.set_yscale('log')
+    ax.set_ylabel(r"$\langle N_s \rangle$")
+    ax.set_ylim(1e-4, 7)
+    ax.set_title(r"$\langle N_s \rangle$")
+    ax.legend(loc='upper left')
     return fig 
 
 def plot_varying_log10M1():
     fig, ax1 = plt.subplots(figsize=(10, 8))
     log10M1_array = np.linspace(12, 16, 11)
     for i, log10M1 in enumerate(log10M1_array):
+        if i == 0 or i == len(log10M1_array)-1:
+            ls = "solid"
+            alpha_ = 1.0
+        else:
+            ls = "dashed"
+            alpha_ = 0.7
+        if i == len(log10M1_array)-1:
+            color_ = "blue"
+        else:
+            color_ = colors[i]
+
         ns = N_s(logM, alpha, kappa, sigma_logM, log10_Mmin, log10M1)
-        ax1.plot(logM, ns, color=colors[i], label=rf"$\log_{{10}} M_1={log10M1:.1f}$")
+        ax1.plot(logM, ns, ls=ls, alpha=alpha_, color=color_, label=rf"$\log_{{10}} M_1={log10M1:.1f}$")
     fig.suptitle(r"Varying $\log_{10} M_1$")
     ax1.set_xlabel(XLABEL)
     ax1.set_ylabel(r"$\langle N_s \rangle$")
@@ -113,8 +157,19 @@ def plot_varying_kappa():
     fig, ax1 = plt.subplots(figsize=(10, 8))
     kappa_array = np.linspace(0.1, 1.1, 11)
     for i, kappa in enumerate(kappa_array):
+        if i == 0 or i == len(kappa_array)-1:
+            ls = "solid"
+            alpha_ = 1.0
+        else:
+            ls = "dashed"
+            alpha_ = 0.7
+        if i == len(kappa_array)-1:
+            color_ = "blue"
+        else:
+            color_ = colors[i]
+
         ns = N_s(logM, alpha, kappa, sigma_logM, log10_Mmin, log10M1)
-        ax1.plot(logM, ns, color=colors[i], label=rf"$\kappa={kappa:.1f}$")
+        ax1.plot(logM, ns, ls=ls, alpha=alpha_, color=color_, label=rf"$\kappa={kappa:.1f}$")
     fig.suptitle(r"Varying $\kappa$")
     ax1.set_xlabel(XLABEL)
     ax1.set_ylabel(r"$\langle N_s \rangle$")
@@ -129,8 +184,19 @@ def plot_varying_alpha():
     fig, ax1 = plt.subplots(figsize=(10, 8))
     alpha_array = np.linspace(0.1, 1.1, 11)
     for i, alpha in enumerate(alpha_array):
+        if i == 0 or i == len(alpha_array)-1:
+            ls = "solid"
+            alpha_ = 1.0
+        else:
+            ls = "dashed"
+            alpha_ = 0.7
+        if i == len(alpha_array)-1:
+            color_ = "blue"
+        else:
+            color_ = colors[i]
+
         ns = N_s(logM, alpha, kappa, sigma_logM, log10_Mmin, log10M1)
-        ax1.plot(logM, ns, color=colors[i], label=rf"$\alpha={alpha:.1f}$")
+        ax1.plot(logM, ns, ls=ls, alpha=alpha_, color=color_, label=rf"$\alpha={alpha:.1f}$")
     fig.suptitle(r"Varying $\alpha$")
     ax1.set_xlabel(XLABEL)
     ax1.set_ylabel(r"$\langle N_s \rangle$")
@@ -139,27 +205,42 @@ def plot_varying_alpha():
     ax1.set_xlim(13,15.1)
     ax1.legend(loc='lower right')
     return fig 
-SAVE = False 
 
 def plot_sigma_logM(save=SAVE):
     sigma_logM = plot_varying_sigma_logM()
     if save:
         title = Path(f"./plots/occ_varying_sigma_logM.png")
         print(f"Saving to {title}")
+        sigma_logM.tight_layout()
         sigma_logM.savefig(title, dpi=200)
         sigma_logM.clf()
     else:
         plt.show()
+        plt.close()
+
     
-def plot_logMmin(save=SAVE):
-    logMmin = plot_varying_log10_Mmin()
+def plot_logMmin_Nc(save=SAVE):
+    logMmin = plot_varying_log10_Mmin_Nc()
     if save:
-        title = Path(f"./plots/occ_varying_logMmin.png")
+        title = Path(f"./plots/occ_varying_logMmin_Nc.png")
         print(f"Saving to {title}")
         logMmin.savefig(title, dpi=200)
         logMmin.clf()
     else:
         plt.show()
+        plt.close()
+
+def plot_logMmin_Ns(save=SAVE):
+    logMmin = plot_varying_log10_Mmin_Ns()
+    if save:
+        title = Path(f"./plots/occ_varying_logMmin_Ns.png")
+        print(f"Saving to {title}")
+        logMmin.savefig(title, dpi=200)
+        logMmin.clf()
+    else:
+        plt.show()
+        plt.close()
+
 
 def plot_logM1(save=SAVE):
     logM1 = plot_varying_log10M1()
@@ -170,6 +251,8 @@ def plot_logM1(save=SAVE):
         logM1.clf()
     else:
         plt.show()
+        plt.close()
+
 
 def plot_kappa(save=SAVE):
     kappa = plot_varying_kappa()
@@ -180,6 +263,8 @@ def plot_kappa(save=SAVE):
         kappa.clf()
     else:
         plt.show()
+        plt.close()
+
 
 def plot_alpha(save=SAVE):
     alpha = plot_varying_alpha()
@@ -190,14 +275,17 @@ def plot_alpha(save=SAVE):
         alpha.clf()
     else:
         plt.show()
+        plt.close()
+
 
 
 
 def plotall(save=False):
     plot_sigma_logM(save=save)
-    plot_logMmin(save=save)
+    plot_logMmin_Nc(save=save)
+    plot_logMmin_Ns(save=save)
     plot_logM1(save=save)
     plot_kappa(save=save)
     plot_alpha(save=save)
-# plotall(save=True)
-plotall(save=False)
+plotall(save=True)
+# plotall(save=False)
