c8d3ef599aacdf498c217db1411cabf9ffaeecef
Vikenes <vetle.vkns@gmail.com>
2024-03-08 14:31:33 +0100

    minor

--- HaloModel/wp_example/measure_wp.py
+++ HaloModel/wp_example/measure_wp.py
@@ -26,12 +26,7 @@ for dataflag in ['real', 'redshift']:
             pos = np.load(f'/mn/stornext/d8/data/chengzor/void_abacussummit/data/multi/HOD_LOWZ_gal_pos_cos0_ph{phidx}_z0.25.npy')
         if (dataflag == 'redshift'):
             pos = np.load(f'/mn/stornext/d8/data/chengzor/void_abacussummit/data/multi/HOD_LOWZ_gal_pos_zspace_cos0_ph{phidx}_z0.25.npy')
-        # print(f"{pos[:,0]=}")
-        # print(f"{pos[:,1]=}")
-        # print(f"{pos[:,2]=}")
-        print(f"{pos.shape=}")
-
-        exit()
+        
         edges = (r_binedge,)
         results_wp = wp(
             boxsize=box_size, 
