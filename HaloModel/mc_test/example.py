02add36378aa130b5455132cd3bc305a662fa003
Vikenes <vetle.vkns@gmail.com>
2024-03-08 09:10:31 +0100

    added mcmc example

--- HaloModel/mc_test/example.py
+++ HaloModel/mc_test/example.py
@@ -79,3 +79,80 @@ def plot_ML():
     plt.ylabel("y")
     plt.show()
 
+def log_prior(theta):
+    m, b, log_f = theta
+    if -5.0 < m < 0.5 and 0.0 < b < 10.0 and -10.0 < log_f < 1.0:
+        return 0.0
+    return -np.inf
+
+def log_probability(theta, x, y, yerr):
+    lp = log_prior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + log_likelihood(theta, x, y, yerr)
+
+import emcee
+
+pos = soln.x + 1e-4 * np.random.randn(32, 3)
+nwalkers, ndim = pos.shape
+
+sampler = emcee.EnsembleSampler(
+    nwalkers, ndim, log_probability, args=(x, y, yerr)
+)
+print("Running MCMC...") # SELF 
+sampler.run_mcmc(pos, 2000, progress=True)
+
+labels = ["m", "b", "log(f)"]
+def plot_chain():
+    fig, axes = plt.subplots(3, figsize=(10, 7), sharex=True)
+    samples = sampler.get_chain()
+    for i in range(ndim):
+        ax = axes[i]
+        ax.plot(samples[:, :, i], "k", alpha=0.3)
+        ax.set_xlim(0, len(samples))
+        ax.set_ylabel(labels[i])
+        ax.yaxis.set_label_coords(-0.1, 0.5)
+
+    axes[-1].set_xlabel("step number")
+    plt.show()
+
+tau = sampler.get_autocorr_time()
+print(f"{tau=}")
+
+flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+
+
+mmm = flat_samples[..., 0]
+plt.hist(mmm, bins=100, histtype="step")
+plt.show()
+print(mmm)
+print(mmm.shape)
+exit()
+
+import corner
+
+fig = corner.corner(
+    flat_samples, labels=labels, truths=[m_true, b_true, np.log(f_true)]
+)
+plt.show()
+
+inds = np.random.randint(len(flat_samples), size=100)
+for ind in inds:
+    sample = flat_samples[ind]
+    plt.plot(x0, np.dot(np.vander(x0, 2), sample[:2]), "C1", alpha=0.1)
+plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+plt.plot(x0, m_true * x0 + b_true, "k", label="truth")
+plt.legend(fontsize=14)
+plt.xlim(0, 10)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+from IPython.display import display, Math
+
+for i in range(ndim):
+    mcmc = np.percentile(flat_samples[:, i], [16, 50, 84])
+    q = np.diff(mcmc)
+    txt = "\mathrm{{{3}}} = {0:.3f}_{{-{1:.3f}}}^{{{2:.3f}}}"
+    txt = txt.format(mcmc[1], q[0], q[1], labels[i])
+    display(Math(txt))
