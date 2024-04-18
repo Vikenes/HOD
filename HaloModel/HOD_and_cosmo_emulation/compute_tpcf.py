ef8b7434dfd84a6376b7735cb42b4b836c2362d2
Vikenes <vetle.vkns@gmail.com>
2024-04-18 21:48:26 +0200

    tpcf fin

--- HaloModel/HOD_and_cosmo_emulation/compute_tpcf.py
+++ HaloModel/HOD_and_cosmo_emulation/compute_tpcf.py
@@ -2,24 +2,25 @@ import numpy as np
 import h5py 
 import time 
 from pathlib import Path
-import pandas as pd
-import matplotlib.pyplot as plt
-
+import concurrent.futures
 from pycorr import TwoPointCorrelationFunction
 
 D13_DATA_PATH           = Path("/mn/stornext/d13/euclid_nobackup/halo/AbacusSummit/emulation_files")
 assert D13_DATA_PATH.exists(), f"Error: {D13_DATA_PATH} not found. Check path."
 
+"""
+FIX FLAGS BEING LOOPED OVER  
+"""
 
 class TPCF_ABACUS:
     def __init__(
             self,
             r_bin_edges: np.array,
-            ng_fixed:    bool = True,
+            ng_fixed:    bool,
+            use_sep_avg: bool,
             boxsize:     float = 2000.0,
             engine:      str   = 'corrfunc',
             nthreads:    int   = 128,
-            use_sep_avg: bool  = False,
             ):
         
         self.r_bin_edges    = r_bin_edges
@@ -33,13 +34,9 @@ class TPCF_ABACUS:
 
         dataset_names = ['train', 'test', 'val']
         if ng_fixed:
-            ng_suffix = "_ng_fixed"
+            self.ng_suffix = "_ng_fixed"
         else:
-            ng_suffix = ""
-        self.fname_suffix    = [f"{flag}{ng_suffix}" for flag in dataset_names]
-        self.halocat_fnames  = [f"halocat_{suffix}.hdf5" for suffix in self.fname_suffix]
-        self.TPCF_fnames     = [f"TPCF_{suffix}.hdf5" for suffix in self.fname_suffix]
-
+            self.ng_suffix = ""
 
     def compute_TPCF_from_gal_pos_with_sepavg(
             self,
@@ -88,27 +85,36 @@ class TPCF_ABACUS:
     
     def save_TPCF_data_from_HOD_catalogue(
             self,
-            version:    int  = 0,
-            phase:      int  = 0,
+            version: int,
+            phase:   int,
+            flags:   list,
     ):
         SIM_DATA_PATH       = Path(D13_DATA_PATH / f"AbacusSummit_base_c{str(version).zfill(3)}_ph{str(phase).zfill(3)}")
         HOD_CATALOGUE_PATH  = Path(SIM_DATA_PATH / "HOD_catalogues")
         OUTPUT_PATH         = Path(SIM_DATA_PATH / "TPCF_data")
         
-        Path(OUTPUT_PATH).mkdir(parents=False, exist_ok=True)
+        Path(OUTPUT_PATH).mkdir(parents=False, exist_ok=False)
+        fname_suffix    = [f"{flag}{self.ng_suffix}" for flag in flags]
+        halocat_fnames  = [f"halocat_{suffix}.hdf5" for suffix in fname_suffix]
+        TPCF_fnames     = [f"TPCF_{suffix}.hdf5" for suffix in fname_suffix]
 
-        for TPCF_fname, halocat_fname in zip(self.TPCF_fnames, self.halocat_fnames):
+        for TPCF_fname, halocat_fname in zip(TPCF_fnames, halocat_fnames):
 
             OUTFILE     = f"{OUTPUT_PATH}/{TPCF_fname}"
-
             if Path(OUTFILE).exists():
                 print(f"File {OUTFILE} already exists, skipping...")
                 continue
 
-            halo_file   = h5py.File(f"{HOD_CATALOGUE_PATH}/{halocat_fname}", "r")
+            halo_filename = Path(f"{HOD_CATALOGUE_PATH}/{halocat_fname}")
+            if not halo_filename.exists():
+                # No HOD catalogue with "train" data for c000_ph000-ph024 or c001-c004
+                continue 
+
+            halo_file   = h5py.File(halo_filename, "r")
             N_nodes     = len(halo_file.keys()) # Number of parameter samples used to make catalogue
 
-            print(f"Computing all {N_nodes} {TPCF_fname} for {SIM_DATA_PATH.name}...", end=" ")
+            print(f"Computing all {N_nodes} {TPCF_fname} for {SIM_DATA_PATH.name}...")
+
             t0 = time.time()
             fff = h5py.File(OUTFILE, "w")
             # Compute TPCF for each node
@@ -135,10 +141,10 @@ class TPCF_ABACUS:
         
     def save_TPCF_all_versions(
             self,
-            c000_phases:            bool = False,
-            c001_c004:              bool = False,
-            linear_derivative_grid: bool = False,
-            broad_emulator_grid:    bool = False,
+            c000_phases:            bool,
+            c001_c004:              bool,
+            linear_derivative_grid: bool,
+            broad_emulator_grid:    bool,
         ):
         """
         With nthreads=128, each TPCF takes roughly 1s to compute. 
@@ -153,28 +159,110 @@ class TPCF_ABACUS:
             for phase in phases:
                 self.save_TPCF_data_from_HOD_catalogue(
                     version = 0, 
-                    phase   = phase)
+                    phase   = phase,
+                    flags   = ["test", "val"])
             
         if c001_c004:
             versions = np.arange(1, 5)
             for version in versions:
                 self.save_TPCF_data_from_HOD_catalogue(
                     version = version, 
-                    phase   = 0)
+                    phase   = 0,
+                    flags   = ["test", "val"])
         
         if linear_derivative_grid:
             versions = np.arange(100, 127)
             for version in versions:
                 self.save_TPCF_data_from_HOD_catalogue(
                     version = version, 
-                    phase   = 0)
+                    phase   = 0,
+                    flags   = ["train", "test", "val"])
         
         if broad_emulator_grid:
             versions = np.arange(130, 182)
             for version in versions:
                 self.save_TPCF_data_from_HOD_catalogue(
                     version = version, 
-                    phase   = 0)
+                    phase   = 0,
+                    flags   = ["train", "test", "val"])
+                
+    def save_TPCF_linear_derivative_grid(self, start=100, stop=127):
+        versions = np.arange(start, stop)
+        for version in versions:
+            self.save_TPCF_data_from_HOD_catalogue(
+                version = version, 
+                phase   = 0,
+                flags   = ["train", "test", "val"])
+
+    def save_TPCF_broad_emulator_grid(self, start=130, stop=182):
+        versions = np.arange(start, stop)
+        for version in versions:
+            self.save_TPCF_data_from_HOD_catalogue(
+                version = version, 
+                phase   = 0,
+                flags   = ["train", "test", "val"])
+
+
+
+def run_c000_parallel(r_bin_edges, nthreads):
+    tt = TPCF_ABACUS(
+        r_bin_edges=r_bin_edges, 
+        ng_fixed=False,
+        nthreads=nthreads,
+        use_sep_avg=True,
+        )
+    phases   = np.arange(0, 25)
+    t000 = time.time()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(
+            tt.save_TPCF_data_from_HOD_catalogue, 
+            [0 for phase in phases],
+            phases,
+            [["test", "val"] for phase in phases]
+            )
+    print(f"Done with c000. Took {time.time() - t000:.2f} s")
+        
+    
+
+def run_c001_c004_parallel(r_bin_edges, nthreads=32):
+    tt = TPCF_ABACUS(
+        r_bin_edges=r_bin_edges, 
+        ng_fixed=False,
+        nthreads=nthreads,
+        use_sep_avg=True,
+        )
+    versions   = np.arange(1, 5)
+    t000 = time.time()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(
+            tt.save_TPCF_data_from_HOD_catalogue, 
+            versions,
+            [0 for _ in versions],
+            [["test", "val"] for _ in versions]
+            )
+        
+    print(f"Done with c001-c004. Took {time.time() - t000:.2f} s")
+
+
+def run_lin_der_grid_parallel(r_bin_edges, start, stop, nthreads):
+    tt = TPCF_ABACUS(
+        r_bin_edges=r_bin_edges, 
+        ng_fixed=False,
+        nthreads=nthreads,
+        use_sep_avg=True,
+        )
+    assert start >= 100 and stop <= 127 and start < stop, f"Error: start={start} and stop={stop} not valid for linear derivative grid."
+    versions   = np.arange(start, stop)
+    t000 = time.time()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(
+            tt.save_TPCF_data_from_HOD_catalogue, 
+            versions,
+            [0 for _ in versions],
+            [["train", "test", "val"] for _ in versions]
+            )
+        
+    print(f"Done with c{start}-c{stop}. Took {time.time() - t000:.2f} s")
 
 # Use same bins as Cuesta-Lazaro et al.
 r_bin_edges = np.concatenate((
@@ -185,22 +273,29 @@ r_bin_edges = np.concatenate((
             
 tt = TPCF_ABACUS(
     r_bin_edges=r_bin_edges, 
-    ng_fixed=True,
+    ng_fixed=False,
     nthreads=128,
     use_sep_avg=True,
     )
 
 
-#tt.save_TPCF_all_versions(
-#    c000_phases=False,
-#    c001_c004=False,
-#    linear_derivative_grid=True,
-#    broad_emulator_grid=False,
-#)
+# tt.save_TPCF_linear_derivative_grid(108, 127)
+# tt.save_TPCF_broad_emulator_grid(130, 156)
+# tt.save_TPCF_broad_emulator_grid(156, 182)
+
 
 # tt.save_TPCF_all_versions(
-#     c000_phases=False,
-#     c001_c004=False,
-#     linear_derivative_grid=False,
-#     broad_emulator_grid=True,
-# )
+#     c000_phases            = False,
+#     c001_c004              = False,
+#     linear_derivative_grid = True,
+#     broad_emulator_grid    = False,
+#     )
+
+
+
+# run_c000_parallel(r_bin_edges)
+# run_c001_c004_parallel(r_bin_edges)
+# run_lin_der_grid_parallel(r_bin_edges, 100, 104, nthreads=32)
+# run_lin_der_grid_parallel(r_bin_edges, 104, 106, nthreads=64)
+# run_lin_der_grid_parallel(r_bin_edges, 104, 108, nthreads=64)
+
