f114de2098ea8fd0a182caf3358cc1b5a30ff2c0
Vikenes <vetle.vkns@gmail.com>
2024-04-20 16:34:44 +0200

    removed random state which gave all samples identical hod parameters

--- HaloModel/HOD_and_cosmo_emulation/generate_HOD_parameters_varying_ng.py
+++ HaloModel/HOD_and_cosmo_emulation/generate_HOD_parameters_varying_ng.py
@@ -11,6 +11,30 @@ from numdens_HOD import get_log10Mmin_from_varying_log_ng
 D13_BASE_PATH       = "/mn/stornext/d13/euclid_nobackup/halo/AbacusSummit"
 D13_EMULATION_PATH  = f"{D13_BASE_PATH}/emulation_files"
 
+dataset_config_size = {
+        'train': 500,
+        'test':  100,
+        'val':   100
+        }
+
+sigma_logM  = 0.6915 
+log10M1     = 14.42 # h^-1 Msun
+kappa       = 0.51 
+alpha       = 0.9168  
+param_limits = np.array([
+    [sigma_logM * 0.9,  sigma_logM  * 1.1],
+    [log10M1    * 0.9,  log10M1     * 1.1],
+    [kappa      * 0.9,  kappa       * 1.1],
+    [alpha      * 0.9,  alpha       * 1.1],
+    [-3.7            , -3.2              ],
+    ])
+LHC_sampler = LHS(
+    xlimits      = param_limits, 
+    criterion    = "corr", 
+    ) 
+
+
+
 def store_fiducial_HOD_parameters():
     
     sigma_logM  = 0.6915 
@@ -51,11 +75,9 @@ def store_fiducial_HOD_parameters():
 
 
 def make_csv_files(
-        num_train:  int   = 500, 
-        num_test:   int   = 100, 
-        num_val:    int   = 100, 
-        version:    int   = 0,
-        phase:      int   = 0,
+        version:    int,
+        phase:      int,
+        flags:      list,
         ):
     """
     Create parameter datasets with LHS sampler.
@@ -66,32 +88,9 @@ def make_csv_files(
     If fix_ng=True, log10Mmin is estimated such that the galaxy number density becomes ng_desired.
     """
 
-    ### FIDUCIAL VALUES. From https://arxiv.org/pdf/2208.05218.pdf, Table 3  ###
-    sigma_logM  = 0.6915 
-    log10M1     = 14.42 # h^-1 Msun
-    kappa       = 0.51 
-    alpha       = 0.9168  
-
-
-    # Vary parameters by +/- 10% around fiducial values 
-    param_limits = np.array([
-        [sigma_logM * 0.9,  sigma_logM  * 1.1],
-        [log10M1    * 0.9,  log10M1     * 1.1],
-        [kappa      * 0.9,  kappa       * 1.1],
-        [alpha      * 0.9,  alpha       * 1.1],
-        [-3.7            , -3.2              ],
-    ])
-
-    
-    dataset_config_size = {
-        'train': num_train,
-        'test':  num_test,
-        'val':   num_val
-        }
-    dataset_names = ['train', 'test', 'val']
 
     # Create parameter files. 
-    for dataset in dataset_names:
+    for dataset in flags:
         version_str = str(version).zfill(3)
         phase_str   = str(phase).zfill(3)
         simname     = f"AbacusSummit_base_c{version_str}_ph{phase_str}"
@@ -103,14 +102,14 @@ def make_csv_files(
         fname   = Path(f"HOD_parameters_{dataset}.csv")
         outfile = Path(HOD_PARAMETERS_PATH / fname)
         if outfile.exists():
-            print(f"File {outfile.name} already exists. Skipping.")
-            continue
+            print(f"WARNING!!!!!!!!!!!!!!!!!!! {outfile.name} exists. Skipping.")
+            # continue
+            input("..... ???")
 
         # Create LHS sampler
         LHC_sampler = LHS(
             xlimits      = param_limits, 
             criterion    = "corr", 
-            random_state=0
         ) 
         # Sample parameters
         samples     = dataset_config_size[dataset]
@@ -150,42 +149,48 @@ def make_csv_files(
 
 def make_csv_files_phase_wrapper(phase):
     make_csv_files(
-        num_train   = 500,
-        num_test    = 100,
-        num_val     = 100,
         version     = 0,
         phase       = phase,
+        flags       = ['test', 'val'],
     )
+
+def make_csv_files_version_wrapper_without_train(version):
+    make_csv_files(
+        version     = version,
+        phase       = 0,
+        flags       = ['test', 'val'],
+    )
+
 
 def make_csv_files_version_wrapper(version):
     make_csv_files(
-        num_train   = 500,
-        num_test    = 100,
-        num_val     = 100,
         version     = version,
         phase       = 0,
+        flags       = ['train', 'test', 'val'],
     )
 
-
-def make_csv_files_broad_emulator_grid(parallel=True):
+def make_csv_files_broad_emulator_grid():
     print("Making HOD parameter files for broad emulator grid, versions 130-181.")
     version_range = range(130,182)
     with concurrent.futures.ProcessPoolExecutor() as executor:
         executor.map(make_csv_files_version_wrapper, [i for i in version_range])
     print("Done.")
 
-def make_csv_files_linear_derivative_grid(parallel=True, include_c00_=True):
-    if include_c00_:
-        version_range = np.arange(1,5)
-        version_range = np.concatenate((version_range, np.arange(100,127)))
-    else:
-        version_range = range(100,127)
+def make_csv_files_c001_c004():
+    print("Making HOD parameter files for c001-c004, versions 001-004.")
+    version_range = np.arange(1,5)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(make_csv_files_version_wrapper_without_train, [i for i in version_range])
+
+def make_csv_files_linear_derivative_grid():
+    version_range = range(100,127)
     print("Making HOD parameter files for linear derivative grid, versions 100-126.")
+    
     with concurrent.futures.ProcessPoolExecutor() as executor:
         executor.map(make_csv_files_version_wrapper, [i for i in version_range])
     print("Done.")
             
-def make_csv_files_c000_all_phases(parallel=True):
+def make_csv_files_c000_all_phases():
     phase_range = range(0,25)
     print("Making HOD parameter files for all phases of c000, phases 000-024.")
     with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -195,6 +200,7 @@ def make_csv_files_c000_all_phases(parallel=True):
 
 
 
-# make_csv_files_c000_all_phases(parallel=True)
-# make_csv_files_linear_derivative_grid(parallel=True, include_c00_=True)
-# make_csv_files_broad_emulator_grid(parallel=True)
+# make_csv_files_c000_all_phases()
+# make_csv_files_c001_c004()
+# make_csv_files_linear_derivative_grid()
+# make_csv_files_broad_emulator_grid()
