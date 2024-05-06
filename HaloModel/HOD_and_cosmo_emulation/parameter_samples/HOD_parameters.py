98c9b9df324c6bc59db8cde89fc4b798e6d38732
Vikenes <vetle.vkns@gmail.com>
2024-05-06 11:10:24 +0200

    store histogram plot of log10Mmin

--- HaloModel/HOD_and_cosmo_emulation/parameter_samples/HOD_parameters.py
+++ HaloModel/HOD_and_cosmo_emulation/parameter_samples/HOD_parameters.py
@@ -7,82 +7,98 @@ import h5py
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
+from HOD_and_cosmo_prior_ranges import get_fiducial_HOD_params
 
 matplotlib.rcParams['text.usetex'] = True
 matplotlib.rcParams.update({'font.size': 14})
 matplotlib.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+
 params = {'xtick.top': True, 
           'ytick.right': True, 
           'xtick.direction': 'in', 
           'ytick.direction': 'in',
-          "xtick.labelsize": 8,
-          "ytick.labelsize": 8,
-          "xtick.major.size": 2,
-          "xtick.minor.visible": True,
-          "xtick.minor.size": 1,
-          "ytick.major.size": 2,
-          "ytick.minor.visible": True,
-          "ytick.minor.size": 1,
           }
 plt.rcParams.update(params)
 
-BASEPATH            = Path("/mn/stornext/d5/data/vetleav/emulation_data/TPCF_HOD_and_cosmo")
-EMULPATH            = Path(BASEPATH / "xi_over_xi_fiducial")
-D13_FIDUCIAL_PATH   = Path("/mn/stornext/d13/euclid_nobackup/halo/AbacusSummit/emulation_files/AbacusSummit_base_c000_ph000/HOD_parameters")
+DATAPATH            = Path("/mn/stornext/d13/euclid_nobackup/halo/AbacusSummit/emulation_files")
 
 DATASET_NAMES       = ["train", "test", "val"]
-HOD_PARAM_NAMES     = ["alpha", "kappa", "log10M1", "log10Mmin", "sigma_logM"]
+HOD_PARAM_NAMES     = ["alpha", "kappa", "log10M1", "log10Mmin", "sigma_logM", "log10_ng"]
 
 
-def save_HOD_params_array():
+N_SIMS = len([simulation for simulation in DATAPATH.glob("AbacusSummit*")])
+
+def save_HOD_params():
+    """
+    Store HOD_parameters_{flag}. csv files for each flag  
+    """
     for flag in DATASET_NAMES:
+        # Loop over flag. Skip file if exists 
         outfile     = Path(f"./HOD_params_{flag}.csv")
         if outfile.exists():
             print(f"{outfile} already exists, skipping")
             continue
-        fff         = h5py.File(EMULPATH / f"TPCF_{flag}_ng_fixed.hdf5", "r")
-        N_sims      = len([fff[simulation] for simulation in fff.keys() if simulation.startswith("AbacusSummit")])
-        for key in fff.keys():
-            if not key.startswith("AbacusSummit"):
-                continue
-            N_nodes = len(fff[key].keys())
-            break 
+
+        # Empty dataframe to fill with HOD parameters
+        HOD_params = pd.DataFrame()
+        for simulation in DATAPATH.glob("AbacusSummit*"):
+            # Loop over all simulations
+            if not Path(simulation / "HOD_parameters" / f"HOD_parameters_{flag}.csv").exists():
+                # Skip simulation if it doesn't contain data for this flag
+                continue 
+
+            # Concatenate HOD parameters from simulations
+            HOD_params = pd.concat([HOD_params, pd.read_csv(simulation / "HOD_parameters" / f"HOD_parameters_{flag}.csv")])
         
-        HOD_params  = np.zeros((N_sims, N_nodes, len(HOD_PARAM_NAMES)))
-        for ii, simulation in enumerate(fff.keys()):
-            if not simulation.startswith("AbacusSummit"):
-                continue
-            fff_cosmo = fff[simulation]#["node0"] 
-            for jj, node in enumerate(fff_cosmo.keys()):
-                fff_cosmo_node = fff_cosmo[node]
-                HOD_params[ii,jj] = np.array([fff_cosmo_node.attrs[param] for param in HOD_PARAM_NAMES])
+        # Save HOD parameters to file
+        HOD_params.to_csv(outfile, index=False)
 
-        # Reshape HOD_params array to (N_sims*N_nodes, N_params)
-        HOD_params = HOD_params.reshape(-1, len(HOD_PARAM_NAMES))
-
-        # Save to csv file
-        df = pd.DataFrame({
-            key: HOD_params[:, kk] for kk, key in enumerate(HOD_PARAM_NAMES)
-        })
-        df.to_csv(outfile, index=False)
-
-def load_HOD_params_array():
-    HOD_params_dict = {}
+def save_combined_HOD_params():
+    # For testing initially. 
+    HOD_params_all = pd.DataFrame()
     for flag in DATASET_NAMES:
-        df = pd.read_csv(f"./HOD_params_{flag}.csv")
-        HOD_params_dict[flag] = {
-            key: df[key].values for key in HOD_PARAM_NAMES
-        }
-        hpdf = HOD_params_dict[flag]
-    
-    df_fiducial = pd.read_csv(D13_FIDUCIAL_PATH / "HOD_parameters_fiducial_ng_fixed.csv")
-    HOD_params_dict["fiducial"] = {
-        key: df_fiducial[key].values for key in HOD_PARAM_NAMES
+        HOD_params_all = pd.concat([HOD_params_all, pd.read_csv(f"./HOD_params_{flag}.csv")])
+    HOD_params_all.to_csv("HOD_params_all.csv", index=False)
+
+def plot_log10Mmin():
+    """
+    Make histogram showing the distribution of log10Mmin for each dataset
+    """
+    fig, ax = plt.subplots()
+    flags = {
+        "train": "Training",
+        "test": "Testing",
+        "val": "Validation",
     }
+    for flag in DATASET_NAMES:
+        HOD_params = pd.read_csv(f"HOD_params_{flag}.csv")
 
-    return HOD_params_dict
+        ax.hist(HOD_params["log10Mmin"], bins=100, histtype="step", label=flags[flag])
+    
+    ax.legend()
+    ax.set_xlabel(r"$\log{M_\mathrm{min}}$")
+    ax.set_ylabel("Number of samples")
+    if not SAVEFIG:
+        plt.show()
+        return 
+    
 
-HOD_params_dict = load_HOD_params_array()
+    figname_stem = "log10Mmin_hist"
+    figname_png = Path(f"plots/thesis_figures_HOD/{figname_stem}.png")
+    figname_pdf = Path(f"plots/thesis_figures_HOD/{figname_stem}.pdf")
+    print(f"Storing figure {figname_png}")
+    fig.savefig(
+        figname_png, 
+        bbox_inches="tight", 
+        dpi=200
+        )
+    print(f"Storing figure {figname_pdf}")
+    fig.savefig(
+        figname_pdf, 
+        bbox_inches="tight"
+        )
 
+global SAVEFIG
+SAVEFIG = False
 
-HOD_PARAM_NAMES_LABELS = [r"$\alpha", r"$\kappa$", r"$\log_{10}M_1$", r"$\log_{10}M_{\mathrm{min}}$", r"$\sigma_{\log M}$"]
+plot_log10Mmin()
